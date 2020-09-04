ed9d157fc52ed4ca17de3e9b333c742755b55ce7
Siham AISSAOUI <6931712-SIhamAis@users.noreply.gitlab.com>
2020-09-04 17:45:40 +0200

    Initial commit

---
+++ src/app/main/differentiators/differentiators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-differentiators',
+  templateUrl: './differentiators.component.html',
+  styleUrls: ['./differentiators.component.scss']
+})
+export class DifferentiatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
