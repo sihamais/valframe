4274cf7806221aaafbcbab021c41a9cf000ed6ce
Paco KLEITZ <pacokleitz@pm.me>
2020-09-16 00:42:38 +0200

    deleted duplicate

--- src/app/models/node.model.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from './type.model';
-
-export class Node{
-    constructor(public id: number,
-        public child: number[],
-        public parent: number,
-        public type: Type){}
-}
